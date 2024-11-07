681864dc0887b3cf03cfb4f4eae1a0812502fcdb
LeonardoC <leonardo_castilho1@hotmail.com>
2024-11-07 13:09:03 -0300

    Commit inicial de criação do projeto

---
+++ src/main/java/br/com/leonardo/atividade_elotech/AtividadeElotechApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.atividade_elotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeElotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeElotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leonardo/atividade_elotech/AtividadeElotechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.atividade_elotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeElotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
