006ba0cc377069cf6e90c27c45c0a22416435d6f
CastilhoLeo <leonardo_castilho1@hotmail.com>
2025-03-04 21:50:21 -0300

    Finalizando liquibase

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,24 +7,24 @@ INSERT INTO usuario(
 
 INSERT INTO livro(
 	data_publicacao, autor, categoria, isbn, titulo)
-	VALUES ('2024-10-15', 'Autor 1', 'AVENTURA', '1', 'Livro 1'),
-	('2024-10-15', 'Autor 2', 'AVENTURA', '2', 'Livro 2'),
-	('2024-10-15', 'Autor 3', 'AVENTURA', '3', 'Livro 3'),
-	('2024-10-15', 'Autor 4', 'AVENTURA', '4', 'Livro 4'),
-	('2024-10-15', 'Autor 5', 'ROMANCE', '5', 'Livro 5'),
-	('2024-10-15', 'Autor 6', 'ROMANCE', '6', 'Livro 6'),
-	('2024-10-15', 'Autor 7', 'ROMANCE', '7', 'Livro 7'),
-	('2024-10-15', 'Autor 8', 'FICCAO', '8', 'Livro 8'),
-	('2024-10-15', 'Autor 9', 'FICCAO', '9', 'Livro 9'),
-	('2024-10-15', 'Autor 10', 'FICCAO', '10', 'Livro 10');
+	VALUES ('2024-10-15', 'Autor 1', 'AVENTURA', '1234567891234', 'Livro 1'),
+	('2024-10-15', 'Autor 2', 'AVENTURA', '2123456789123', 'Livro 2'),
+	('2024-10-15', 'Autor 3', 'AVENTURA', '3123456789123', 'Livro 3'),
+	('2024-10-15', 'Autor 4', 'AVENTURA', '4123456789123', 'Livro 4'),
+	('2024-10-15', 'Autor 5', 'ROMANCE', '5123456789123', 'Livro 5'),
+	('2024-10-15', 'Autor 6', 'ROMANCE', '6123456789123', 'Livro 6'),
+	('2024-10-15', 'Autor 7', 'ROMANCE', '7123456789123', 'Livro 7'),
+	('2024-10-15', 'Autor 8', 'FICCAO', '8123456789123', 'Livro 8'),
+	('2024-10-15', 'Autor 9', 'FICCAO', '9123456789123', 'Livro 9'),
+	('2024-10-15', 'Autor 10', 'FICCAO', '1012345678912', 'Livro 10');
 
 INSERT INTO emprestimo(
 	data_devolucao, data_emprestimo, livro_id, usuario_id, status)
-	VALUES 	('2024-11-09', '2024-11-09', 1, 1, 'DEVOLVIDO'),
-	('2024-11-09', '2024-11-09', 2, 1, 'DEVOLVIDO'),
-	('2024-11-09', '2024-11-09', 3, 1, 'DEVOLVIDO'),
-	('2024-11-09', '2024-11-09', 3, 2, 'DEVOLVIDO'),
-	('2024-11-09', '2024-11-09', 4, 3, 'EMPRESTADO');
+	VALUES 	('2024-11-09', '2024-10-09', 1, 1, 'DEVOLVIDO'),
+	('2024-11-09', '2024-10-09', 2, 1, 'DEVOLVIDO'),
+	('2024-11-09', '2024-10-09', 3, 1, 'DEVOLVIDO'),
+	('2024-11-09', '2024-10-09', 3, 2, 'DEVOLVIDO'),
+	(null, '2024-10-09', 4, 3, 'EMPRESTADO');
 
 
 
